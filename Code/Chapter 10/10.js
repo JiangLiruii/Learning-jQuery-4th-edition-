4df5baf7328f51b8db8cf7e50d24dcffe212c553
Big_Panda <lorry.jiang@hotmail.com>
2018-03-10 16:49:24 +0800

    update for Chapter10 exercise

--- Code/Chapter 10/10.js
+++ Code/Chapter 10/10.js
@@ -4,3 +4,76 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+let timer = 0;
+$(document).on('click', '.photo', function(e) {
+  e.preventDefault();
+  if ($(this).hasClass('selected')) {
+    $(this).removeClass('selected');
+  } else {
+    $(this).addClass('selected');
+  }
+  console.log(this);
+});
+$(document).on('pageLoaded',function() {
+  $('#loading').hide();
+})
+$(document).on('nextPage', function(e){
+  let url = $('#more-photos').attr('href');
+  console.log(this);
+  if ($('#loading')) {
+    $('#loading').show();
+  } else {
+    $('<div id="loading">loading</div>').appendTo('#gallery')
+  }
+  $.get(url, function(data) {
+    $(data).appendTo('#gallery');
+    $(this).trigger('pageLoaded');    
+  });
+});
+let pageNum = 1;
+$(document).on('nextPage',function(){
+  pageNum += 1;
+  if(pageNum < 20) {
+    $('#more-photos').attr('href','pages/' + pageNum + '.html'); 
+  } else {
+    $('#more-photos').hide();
+  }
+});
+$(document).on('mouseenter mouseleave', 'div.photo', function(e) {
+  var $details = $(this).find('.details');
+  if (!timer) {
+    timer = setTimeout(function() {
+      console.log(e.pageX, e.pageY);
+      timer = 0;
+    },5000);
+  }
+  
+  if (event.type == 'mouseenter') {
+    $details.fadeTo('fast', 0.7);
+  } else {
+    $details.fadeOut('fast');
+  }
+});
+count = 0;
+$(document).on('tribleClick',function() {
+  count += 1;
+  console.log(count);
+  setTimeout(function() {
+    count = 0;
+  },500);
+  if (count === 3) {
+    $('div#gallery').show();
+  }
+})
+$(document).ready(function() {
+  $('div#gallery').hide();  
+  $('.photo.selected').removeClass('selected');
+  $('#more-photos').click(function(e) {
+    e.preventDefault();
+    $(this).trigger('nextPage');
+  });
+  $('h1').click(function(){
+    console.log(1);
+    $(this).trigger('tribleClick');
+  })
+});
